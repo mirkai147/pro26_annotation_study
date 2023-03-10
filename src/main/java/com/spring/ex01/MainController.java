111551a303df995e7e606a0f263ff2eb84a6518f
mirkai <mirkai@naver.com>
2023-03-10 15:37:18 +0900

    3월 9일 26장 스프링 어노테이션 이용해 url요청 실습

---
+++ src/main/java/com/spring/ex01/MainController.java
@@ -0,0 +1,21 @@
+package com.spring.ex01;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("mainController")
+@RequestMapping("/aaaaaa")
+public class MainController {
+   @RequestMapping(value="/aaaa.do" ,method=RequestMethod.GET)
+   public ModelAndView main1(HttpServletRequest request, HttpServletResponse response)  throws Exception{
+      ModelAndView mav=new ModelAndView();
+      mav.addObject("msg","main1");
+      mav.setViewName("fsdasdafds");
+      return mav;
+   }
+}
